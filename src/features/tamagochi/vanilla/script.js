ace24a4cd71912faa81c6f2032202616ae4d1ddd
AlexLikeiOS <aishelochkov@gmail.com>
2020-10-24 18:56:36 +0300

    Add user page

---
+++ src/features/tamagochi/vanilla/script.js
@@ -0,0 +1,14 @@
+export const main = () => {
+	const profile = document.querySelector('.profile')
+	profile.innerHTML = 'profile'
+	const report1 = document.getElementById('report1')
+	report1.innerHTML = '5 stars'
+	const points1 = document.getElementById("points1")
+	points1.innerHTML = '<span>100</span>'
+	const report2 = document.getElementById('report2')
+	report2.innerHTML = '2 stars'
+	const report3 = document.getElementById('report3')
+	report3.innerHTML = '4.5 stars'
+	var tamagochi = document.querySelector('.tamagochi')
+	tamagochi.innerHTML = "<img src='img/tmp.png' alt=''>"
+}
