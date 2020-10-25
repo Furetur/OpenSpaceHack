9d5295853c8cc81b06b1d4a0b099a8fd169419de
furetur <furetur@gmail.com>
2020-10-25 11:56:09 +0500

    formatted code

--- src/features/reports/components/Report/Report.js
+++ src/features/reports/components/Report/Report.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import {useSelector} from "react-redux";
-import {selectReport} from "../../reports.slice";
+import { useSelector } from 'react-redux'
+import { selectReport } from '../../reports.slice'
 import bugImg from './bug.png'
 import styles from './Report.module.css'
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom'
 
-
-const Report = ({id}) => {
-    const report = useSelector(state => selectReport(state, id))
+const Report = ({ id }) => {
+    const report = useSelector((state) => selectReport(state, id))
     return report != null ? (
         <div className={styles.Report}>
             <div className={styles.image}>
                 <Link to={`/report/${report.id}`}>
-                    <img src={bugImg} alt="Bug"/>
+                    <img src={bugImg} alt="Bug" />
                 </Link>
             </div>
             <div className={styles.content}>
@@ -27,7 +26,9 @@ const Report = ({id}) => {
                 </div>
             </div>
         </div>
-    ) : <span>Loading...</span>
+    ) : (
+        <span>Loading...</span>
+    )
 }
 
 export default Report
